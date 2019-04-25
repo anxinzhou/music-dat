34b2a6b2ccbf0195b082edcf9b9aa54eedc0c13b
Patrick WANG <ruochwang@gmail.com>
2019-04-25 17:37:26 +0800

    t1: home.vue add user's wallet address at nav bar

--- front-end/src/components/home.vue
+++ front-end/src/components/home.vue
@@ -4,6 +4,7 @@
   <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
     <div class="container">
       <a class="navbar-brand  js-scroll-trigger" href="#" v-scroll-to="'#page-top'">Alphaslot</a>
+      <p id="user-addr" class="js-scroll-trigger user-addr" href="#" v-scroll-to="'#page-top'">This is addr</p>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive"
               aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
         Menu
@@ -30,14 +31,12 @@
   </header>
   <div>
     <a id="dat">
-      <vue-dat
-      >
+      <vue-dat>
 
       </vue-dat>
     </a>
     <a id="avatar">
-      <vue-avatar
-      >
+      <vue-avatar>
 
       </vue-avatar>
     </a>
@@ -67,13 +66,20 @@
         }
       }
 
+      // set user address from cookie
+      let userAddr = document.getElementById('user-addr')
+      if (this.$cookies.isKey('account')) {
+        userAddr.innerText = 'Address: '  + this.$cookies.get('account').address
+      } else {
+        userAddr.innerText = 'Not Logged in'
+      }
+
       window.addEventListener('scroll', scrollHandle)
     },
     created: function () {
       // this.account = this.$store.state.account
     },
     beforeCreate: function () {
-
     }
   }
 </script>
